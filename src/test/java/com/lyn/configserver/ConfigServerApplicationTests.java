bc6a68a06ba4f9c00ea59b610e09fc634dcba0c4
lynme <lynmeig@live.com>
2020-11-20 23:48:53 +0800

    first commit of config server

---
+++ src/test/java/com/lyn/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyn.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
